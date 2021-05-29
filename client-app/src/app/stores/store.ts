ef6b47368890dea1c52a96adbad1a300e63d4aa8
Manjul <manjuljain93@gmail.com>
2021-05-29 18:55:07 +0530

    End of section 19

--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -1,5 +1,6 @@
 import { createContext, useContext } from "react";
 import ActivityStore from "./activityStore";
+import CommentStore from "./CommentStore";
 import CommonStore from "./CommonStore";
 import ModalStore from "./modalStore";
 import ProfileStore from "./ProfileStore";
@@ -11,6 +12,7 @@ interface Store {
     userStore: UserStore;
     modalStore: ModalStore;
     profileStore: ProfileStore;
+    commentStore: CommentStore;
 }
 
 export const store: Store = {
@@ -18,7 +20,8 @@ export const store: Store = {
     commonStore: new CommonStore(),
     userStore: new UserStore(),
     modalStore: new ModalStore(),
-    profileStore: new ProfileStore()
+    profileStore: new ProfileStore(),
+    commentStore: new CommentStore()
 }
 
 export const StoreContext = createContext(store);
